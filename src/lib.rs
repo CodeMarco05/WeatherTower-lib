6ad25230cb82003c2fd0ef634d798840f8c96550
CodeMarco05 <brandt.marco05@gmail.com>
2024-09-03 15:13:03 +0200

    updates to fn call

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,30 @@
 
 
+use query_params::QueryParams;
 use reqwest::blocking::ClientBuilder;
+use models::hour_element::Element;
 
-mod query_params;
 
-mod models{
+pub mod query_params;
+
+pub mod models{
     pub mod raw_data;
     pub mod hour_element;
 }
 
 
-pub fn get_json_weather_data(){
+pub fn get_json_weather_data(params: QueryParams) -> Vec<Element>{
     let client = ClientBuilder::new().build().unwrap();
 
     let base_url = "https://api.open-meteo.com/v1/forecast";
 
-    let params = query_params::QueryParams{
+    /*let params = query_params::QueryParams{
         latitude: 47.782,
         longitude: 9.6106,
         hourly: vec!["temperature_2m", "rain", "snowfall", "weather_code"],
         timezone: "Europe/Berlin",
         past_days: 2
-    };
+    };*/
 
     let url = params.to_query_string(base_url);
 
@@ -31,8 +34,9 @@ pub fn get_json_weather_data(){
 
     let result = models::raw_data::gen_json(&body).expect("Error during Json generation.");
 
-    let mut elements = models::hour_element::Element::to_element_vec(&result.hourly).expect("No Vector with Elements returned.");
+    let mut elements = Element::to_element_vec(&result.hourly).expect("No Vector with Elements returned.");
 
     elements.sort_by_key(|e| e.time);
 
+    return elements;
 }
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,7 +1,16 @@
 use weather_tower_lib::get_json_weather_data;
+use weather_tower_lib::query_params::QueryParams;
 
 #[test]
 fn test_get_json_weather_data() {
-	get_json_weather_data();
+	let params = QueryParams {
+		latitude: 47.782,
+		longitude: 9.6106,
+		hourly: vec!["temperature_2m", "rain", "snowfall", "weather_code"],
+		timezone: "Europe/Berlin",
+		past_days: 2,
+	};
+
+	get_json_weather_data(params);
 	// Add assertions or checks here if needed
 }
